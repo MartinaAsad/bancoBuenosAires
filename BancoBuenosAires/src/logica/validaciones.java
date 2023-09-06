95bbacdad9db6b622e8bb40cecfc28713dac7bde
MartinaAsad <lasupermartu15@gmail.com>
2023-09-06 18:59:59 -0300

    agregue los archivos

---
+++ BancoBuenosAires/src/logica/validaciones.java
@@ -0,0 +1,8 @@
+package logica;
+
+public interface validaciones {
+	public boolean verSiEstaVacio(String jtx, String jtx2, String jtx3);//arreglar mas adelante este metodo 
+	public boolean verSiExiste();
+	public boolean verSiEstaVacio(String jtx, String jtx2);
+
+}
