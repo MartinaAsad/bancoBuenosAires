1f4f9479f8682290e37bd9df7b5e87fe48433834
MartinaAsad <lasupermartu15@gmail.com>
2023-09-13 18:34:36 -0300

    metodo altaUsuario

--- BancoBuenosAires/src/logica/validaciones.java
+++ BancoBuenosAires/src/logica/validaciones.java
@@ -4,5 +4,7 @@ public interface validaciones {
 	public boolean verSiEstaVacio(String jtx, String jtx2, String jtx3);//arreglar mas adelante este metodo 
 	public boolean verSiExiste();
 	public boolean verSiEstaVacio(String jtx, String jtx2);
+	public boolean verSiEstaVacio();
+	public int existeUsuario();
 
 }
